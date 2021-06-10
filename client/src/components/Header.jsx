c70c65c3de660980f8afeeb4948b859153af649a
ali yasin <ali99yasin@gmail.com>
2021-06-11 02:14:16 +0300

    completed the non-responsive navbar with css, and added the login layouts with a relatively clean design, still have got the signup and responsive design to do!!! getting tired of css, but hopefully I'm getting a hang of it in the near future

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,33 @@
 import React from 'react'
 const Header = ({ Link, Logo }) => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-            <div className="container">
-                <Link className="navbar-brand" to="/"> <img src={Logo} alt="logo" /></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-menu">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="nav-menu">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link  fs-5" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link class="nav-link dropdown-toggle  fs-5" id="genreDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Genres
-                            </Link>
-                            <ul class="dropdown-menu " aria-labelledby="genreDropdown">
-                                <li><Link class="dropdown-item" href="#">Action</Link></li>
-                                <li><Link class="dropdown-item" href="#">Another action</Link></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><Link class="dropdown-item" href="#">Something else here</Link></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link  fs-5" to="/about">About</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link  fs-5" to="/auth">Sign Up</Link>
-                        </li>
-                    </ul>
-                </div>
+        <header id="header" className="dark-primary-color">
+            <Link className="logo" to="/">
+                <img src={Logo} alt="logo" />
+            </Link>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li>
+                        <Link className="nav-link text-primary-color" to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link text-primary-color" to="/genres">Genres</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link text-primary-color" to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link text-primary-color" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav >
+            <button className="accent-color"><Link to="/auth" className="nav-link text-primary-color">Sign Up</Link></button>
+            <div className="burger-menu">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
             </div>
-        </nav>
+        </header >
     )
 }
 export default Header;
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Auth.css';
+import { Link } from 'react-router-dom'
 
 const Auth = () => {
 	return (
-		<div className="container">
-			<h1> Auth Page</h1>
-		</div>
+		<section className="authentication">
+			<div className="login dark-primary-color" id="login">
+				<h1 className="text-primary-color">Login to your account</h1>
+				<form>
+					<input className="email-input" type="email" required placeholder="example@example.com" />
+					<input className="password-input" type="password" required placeholder="Enter your password" />
+					<p className="divider-color">Don't have an account? <Link to="#sign-up">Sign Up</Link></p>
+					<input className="dark-primary-color btn-submit" type="submit" value="Login" />
+				</form>
+			</div>
+			<div className="sign-up default-primary-color" id="sign-up">
+
+			</div>
+		</section>
 	);
 };
 
